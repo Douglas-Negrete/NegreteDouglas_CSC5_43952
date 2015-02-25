309ba0dc351bd60375635be3f3280a03ce3c01db
card0player <ama824111@gmail.com>
2015-02-25 10:47:32 -0800

    adding a helloWorld program

---
+++ lab/helloWorld/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: douglasnegrete
+ *
+ * Created on February 18, 2015, 9:56 AM
+ *  Purpose: our first program
+ */
+
+//System Libraries
+#include <iostream>//Input/Output Library
+using namespace std;//Standard namespace for the I/O stream library
+
+//user libraries
+
+//global constants
+
+//function prototypes
+
+//execution begins here!
+int main(int argc, char** argv) {
+    
+    //output hello world
+    
+    cout<<"hello world"<<endl;//<----this is an "L" not a one
+
+    //exit the program
+    
+    return 0;
+}
+
