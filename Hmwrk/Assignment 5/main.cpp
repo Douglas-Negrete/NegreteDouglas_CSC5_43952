49fa6ebcae0fe7a6aa89813d3ef5306f9b3a3efe
douglas <douglas@douglas-MacBook>
2015-03-26 03:10:14 -0700

    test commit from netbeans

--- Hmwrk/Assignment 5/main.cpp
+++ Hmwrk/Assignment 5/main.cpp
@@ -205,8 +205,8 @@ int main(int argc, char** argv) {
                     break;
                 }
                 case 'x':{
-                    cout<<"\nThe (a)first problem decides which of two numbers "
-                            "is larger."<<endl;
+                    cout<<"\nThe (a)first problem gives you the sum of an "
+                            "inputed number."<<endl;
                     cout<<"\nThe (b)second problem gives you a roman numeral "
                             "for 1-10."<<endl;
                     cout<<"\nThe (c)third problem decides if a calendar date "
